2fdebf0e4ee79a5fbd200994de0157e599665bc6
Ryan Pham <rtpham10@gmail.com>
2024-03-09 22:45:46 -0800

    Added solve button and display changes to screen

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Calculator operators
 function add(a,b){
     return a + b;
 }
@@ -20,3 +20,36 @@ function calculator(a,b,operator){
     return operator(a,b);
 }
 
+const calcScreen = document.querySelector('.display');
+const buttons = document.querySelectorAll('.buttons');
+
+buttons.forEach((button)=>{
+    
+    button.addEventListener('click', ()=>{
+        if(button.innerText == '0'){
+            calcScreen.innerText = '0';
+        } else if(button.innerText == '1'){
+            calcScreen.innerText = '1';
+        }else if(button.innerText == '2'){
+            calcScreen.innerText = '2';
+        }else if(button.innerText == '3'){
+            calcScreen.innerText = '3';
+        }else if(button.innerText == '4'){
+            calcScreen.innerText = '4';
+        }else if(button.innerText == '5'){
+            calcScreen.innerText = '5';
+        }else if(button.innerText == '6'){
+            calcScreen.innerText = '6';
+        }else if(button.innerText == '7'){
+            calcScreen.innerText = '7';
+        }else if(button.innerText == '8'){
+            calcScreen.innerText = '8';
+        }else if(calcScreen.innerText =='9'){
+            calcScreen.innerText = '9';
+        }//Clear button
+        else{
+            calcScreen.innerText = 0;
+        }
+
+    });
+});
