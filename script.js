7bbf4897f612a6dbff17875f35c4ba6ffe163fec
Ryan Pham <rtpham10@gmail.com>
2024-03-10 17:58:39 -0700

    Numbers are  able to be appended into display screen

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ function calculator(a,b,operator){
     return operator(a,b);
 }
 
-const calcScreen = document.querySelector('.display');
-const numbers = document.querySelector('.numbers');
-const operator = document.querySelector('.operator')
+const calcScreen = document.querySelector('.display'); 
+const numbers = document.querySelector('.numbers'); 
+const operator = document.querySelector('.operator'); 
 
 //value 
 let num1 = [];
@@ -35,12 +35,26 @@ for(let i = 0; i <= 9; i++){
     numbers.appendChild(numberBtn);
 };
 
-//Displays Number and appends it to Array
+//Displays Number and appends it to num1 array for calculation
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button)=>{
     button.addEventListener('click', ()=>{
-        calcScreen.innerText = button.innerText;
-        num1.push(button.innerText)
-        console.log(num1)
+
+        calcScreen.innerText += button.innerText;
+        numberValue.innerText = button.innerText;
+        calcScreen.appendChild(numberValue);
+
+        num1.push(button.innerText);
     });
 });
+
+//Clear Button
+const clearBtn = document.querySelector('.clear')
+clearBtn.addEventListener('click', ()=>{
+    calcScreen.innerText = '0';
+})
+
+
+// operator button merge arr num1 for further calculation
+// after operator every click appends numbers to num2
+// = solves the num with array
