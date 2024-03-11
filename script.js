0bccc257e01f58cde6349d9bcbfe9acf1c792612
Ryan Pham <rtpham10@gmail.com>
2024-03-11 01:26:45 -0700

    Implement functionality for addition operation and result calculation in calculator app

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const calcScreen = document.querySelector('.display');
 const numbers = document.querySelector('.numbers'); 
 const operator = document.querySelector('.operator'); 
 
-//value 
+//values for operation
 let num1 = [];
 let num2 = [];
 
@@ -35,16 +35,33 @@ for(let i = 0; i <= 9; i++){
     numbers.appendChild(numberBtn);
 };
 
-//Displays Number and appends it to num1 array for calculation
+let isFirstClick = true; // Flag to track if it's the first button click
+let isNum1Complete = false; // New flag to check if we're done with num1
+let operation = null; // To keep track of the current operation
+
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button)=>{
-    button.addEventListener('click', ()=>{
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (parseInt(button.innerText) == button.innerText) {
+            if (isFirstClick) {
+                // Clear the initial value and reset the flag
+                calcScreen.innerText = '';
+                isFirstClick = false;
+            }
 
-        calcScreen.innerText += button.innerText;
-        numberValue.innerText = button.innerText;
-        calcScreen.appendChild(numberValue);
+            if (!isNum1Complete) {
+                calcScreen.innerText += button.innerText;
+                num1.push(button.innerText);
+            } else {
+                if (calcScreen.innerText === num1.toString()) {
+                    calcScreen.innerText = '';
+                }
+                calcScreen.innerText += button.innerText;
+                num2.push(button.innerText);
+                console.log(num2);
+            }
+        }
 
-        num1.push(button.innerText);
     });
 });
 
@@ -52,7 +69,39 @@ buttons.forEach((button)=>{
 const clearBtn = document.querySelector('.clear')
 clearBtn.addEventListener('click', ()=>{
     calcScreen.innerText = '0';
+    num1 = [];
+    num2 = [];
+    isFirstClick = true; 
+    isNum1Complete = false; 
+    operation = null; 
 })
+
+//Add button
+const addBtn = document.querySelector('.add')
+addBtn.addEventListener('click', ()=>{
+        num1 = num1.join('');
+        num1 = parseInt(num1);
+        isNum1Complete = true;
+        operation = add;
+        calcScreen.innerText = num1;
+});
+
+//Solve button
+const equalsBtn = document.querySelector('.equals')
+equalsBtn.addEventListener('click', ()=>{
+    num2 = num2.join('');
+    num2 = parseInt(num2);
+    let result = calculator(num1,num2,operation);
+    calcScreen.innerText = result;
+
+    //reset
+    num1 = [];
+    num2 = [];
+    isFirstClick = true; 
+    isNum1Complete = false; 
+    operation = null; 
+    
+});
 
 
 // operator button merge arr num1 for further calculation
